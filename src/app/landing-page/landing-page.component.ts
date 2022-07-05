cbffc7a8902c227bf33d96f528fc12008449c573
Ankit Kumar <ankit.kumar1@scotts.com>
2022-07-05 16:45:42 +0530

    new UI CHANGES

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,9 +6,25 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
+  isMobile!: boolean;
   constructor() { }
 
   ngOnInit(): void {
+    
+   
+
   }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    console.log(window.innerWidth);
+    
+    if(window.innerWidth > 1800) {
+      this.isMobile = true
+    } else {
+      this.isMobile = false
+    }
+    
+}
 
 }
