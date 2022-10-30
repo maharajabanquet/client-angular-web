29a67e7520926f4ddfe034a568e35096924f73a7
Ankit Kumar <ankit.kumar1@scotts.com>
2022-10-30 10:56:41 +0530

    production init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 const LOCAL_PORT = '3000'
 const LOCAL_HOST = `http://localhost:${LOCAL_PORT}/`;
-const HEROKU_UAT = 'https://maharajabanquet.herokuapp.com/';
+const PRODUCTION = 'http://maharajaraxaul.com/';
 
 export const environment = {
   production: false,
-  host: HEROKU_UAT
+  host: PRODUCTION
 };
 
 /*
