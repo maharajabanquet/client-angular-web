65a17d6c6f036e2e66cfa6bdbd5f62806aa878ba
Ankit Kumar <ankit.kumar1@scotts.com>
2022-10-30 10:58:15 +0530

    production hostname change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 const LOCAL_PORT = '3000'
 const LOCAL_HOST = `http://localhost:${LOCAL_PORT}/`;
-const HEROKU_UAT = 'https://maharajabanquet.herokuapp.com/'
+const PRODUCTION = 'http://maharajaraxaul.com/'
 
 export const environment = {
   production: true,
-  host: HEROKU_UAT
+  host: PRODUCTION
 };
